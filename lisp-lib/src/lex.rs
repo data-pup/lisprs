f50338ab587ff3b2af63244d123e80c8122b2d8d
data-pup <kevinmartinucr@gmail.com>
2018-04-07 10:02:16 -0700

    Added some extra details to the test bench, fixed a minor bug in the regex patterns using anchors.

--- lisp-lib/src/lex.rs
+++ lisp-lib/src/lex.rs
@@ -2,8 +2,8 @@ use regex::Regex;
 
 lazy_static! {
     static ref VAR_TOKEN:  Regex = Regex::new(r"[A-Za-z]").unwrap();
-    static ref PAREN_CHAR: Regex = Regex::new(r"[()]").unwrap();
-    static ref OP_CHAR:    Regex = Regex::new(r"[+\-*/]").unwrap();
+    static ref PAREN_CHAR: Regex = Regex::new(r"^[()]$").unwrap();
+    static ref OP_CHAR:    Regex = Regex::new(r"^[+\-*/]$").unwrap();
 }
 
 pub fn get_tokens(input: &str) -> Vec<String> {
@@ -33,7 +33,7 @@ mod lex_tests {
     }
 
     #[test]
-    fn identify_parens() {
+    fn regexes_should_identify_parens() {
         for curr in ["(", ")"].into_iter() {
             let is_match = lex::PAREN_CHAR.is_match(curr);
             assert_eq!(is_match, true, "Did not correctly match: {}", curr);
@@ -43,13 +43,28 @@ mod lex_tests {
             let is_match = lex::PAREN_CHAR.is_match(curr);
             assert_eq!(is_match, false, "Incorrectly matched: {}", curr);
         }
+
+        for curr in ["(+", "hello)"].into_iter() {
+            let is_match = lex::PAREN_CHAR.is_match(curr);
+            assert_eq!(is_match, false, "Incorrectly matched: {}", curr);
+        }
     }
 
     #[test]
-    fn identify_ops() {
+    fn regexes_should_match_operators() {
         for curr in ["+", "-", "*", "/",].into_iter() {
             let is_match = lex::OP_CHAR.is_match(curr);
             assert_eq!(is_match, true, "Did not correctly match: {}", curr);
+        }
+
+        for curr in ["!", ",", ".", "?"].into_iter() {
+            let is_match = lex::OP_CHAR.is_match(curr);
+            assert_eq!(is_match, false, "Incorrectly matched: {}", curr);
+        }
+
+        for curr in ["++", "/hello", "hello/"].into_iter() {
+            let is_match = lex::OP_CHAR.is_match(curr);
+            assert_eq!(is_match, false, "Incorrectly matched: {}", curr);
         }
     }
 }
