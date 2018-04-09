29171f5361ff08c6195e876757287a48666f221b
data-pup <kevinmartinucr@gmail.com>
2018-04-09 12:19:08 -0700

    Lexing now returns validated LispToken vector.

--- lisp-lib/src/lex/mod.rs
+++ lisp-lib/src/lex/mod.rs
@@ -16,62 +16,52 @@ mod lex_tests {
     use lex;
     use lisp_token::{LispToken, OperatorToken};
 
-    // FIXUP: Enable tests once refactoring is complete.
+    #[test]
+    fn get_tokens_should_split_simple_expr_by_whitespace() {
+        let input = "( + 1 1 )";
+        let result = lex::get_tokens(input);
+        let expected = vec![
+            LispToken::OpenExpression,
+                LispToken::Operator(OperatorToken::Add),
+                    LispToken::Value(String::from("1")),
+                    LispToken::Value(String::from("1")),
+            LispToken::CloseExpression,
+        ];
+        assert_eq!(result, expected);
+    }
 
-    // #[test]
-    // fn get_tokens_should_split_simple_expr_by_whitespace() {
-    //     let input = "( + 1 1 )";
-    //     let result = lex::get_tokens(input);
-    //     let expected = vec![
-    //         LispToken::OpenExpression,
-    //             LispToken::Operator(OperatorToken::Add),
-    //                 LispToken::Value(String::from("1")),
-    //                 LispToken::Value(String::from("1")),
-    //         LispToken::CloseExpression,
-    //     ];
-    //     assert_eq!(result, expected);
-    // }
+    #[test]
+    fn get_tokens_should_split_basic_expr_by_whitespace() {
+        let input = "(+ 1 1)";
+        let result = lex::get_tokens(input);
+        let expected = vec![
+            LispToken::OpenExpression,
+                LispToken::Operator(OperatorToken::Add),
+                    LispToken::Value(String::from("1")),
+                    LispToken::Value(String::from("1")),
+            LispToken::CloseExpression,
+        ];
+        assert_eq!(result, expected);
+    }
 
-    // #[test]
-    // fn get_tokens_should_split_basic_expr_by_whitespace() {
-    //     let input = "(+ 1 1)";
-    //     let result = lex::get_tokens(input);
-    //     let expected = vec![
-    //         String::from("("),
-    //             String::from("+"),
-    //                 String::from("1"),
-    //                 String::from("1"),
-    //         String::from(")"),
-    //     ];
-    //     assert_eq!(result, expected);
-    // }
-
-    // #[test]
-    // fn get_tokens_should_handle_a_simple_nested_expression() {
-    //     let input = "(+ (* 1 2) 3 (4))";
-    //     let result = lex::get_tokens(input);
-    //     let expected: Vec<String> = vec![
-    //         "(",
-    //             "+",
-    //                 "(", "*", "1", "2", ")",
-    //                 "3",
-    //                 "(", "4", ")",
-    //         ")",
-    //     ].into_iter().map(str::to_string).collect();
-    //     assert_eq!(result, expected);
-    // }
-
-    // #[test]
-    // fn get_tokens_should_handle_simple_nested_values() {
-    //     let input = "(+ (1))";
-    //     let result = lex::get_tokens(input);
-    //     let expected: Vec<String> = vec![
-    //         "(",
-    //             "+",
-    //                 "(", "1", ")",
-    //         ")",
-    //     ].into_iter().map(str::to_string).collect();
-    //     assert_eq!(result, expected);
-    // }
-
+    #[test]
+    fn get_tokens_should_handle_a_simple_nested_expression() {
+        let input = "(+ (* 1 2) 3 (4))";
+        let result = lex::get_tokens(input);
+        let expected: Vec<LispToken> = vec![
+            LispToken::OpenExpression,
+                LispToken::Operator(OperatorToken::Add),
+                    LispToken::OpenExpression,
+                        LispToken::Operator(OperatorToken::Multiply),
+                        LispToken::Value(String::from("1")),
+                        LispToken::Value(String::from("2")),
+                    LispToken::CloseExpression,
+                    LispToken::Value(String::from("3")),
+                    LispToken::OpenExpression,
+                    LispToken::Value(String::from("4")),
+                    LispToken::CloseExpression,
+            LispToken::CloseExpression,
+        ];
+        assert_eq!(result, expected);
+    }
 }
