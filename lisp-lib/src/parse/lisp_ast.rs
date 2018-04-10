4ec3c66ff3e750464018468364dddb4dbdfebb94
data-pup <kevinmartinucr@gmail.com>
2018-04-09 21:59:03 -0700

    Very basic expression is evaluated by the Expression class. This structure will likely change dramatically, but this will give me something to build around.

--- lisp-lib/src/parse/lisp_ast.rs
+++ lisp-lib/src/parse/lisp_ast.rs
@@ -1,14 +1,46 @@
 use lisp_token::OperatorToken;
 
-struct _Atom<T> {
-    value: T
+#[derive(Debug, PartialEq)]
+struct _Atom { value: i64 }
+
+impl _Atom {
+    pub fn new() -> Self { Self { value: 0_i64 } }
 }
 
-struct _Expression<T> {
+struct _Expression {
     op: OperatorToken,
-    vars: Vec<_Atom<T>>,
+    vars: Vec<_Atom>,
 }
 
-// impl Expression<T> {
-//     pub fn evaluate(&self) -> Atom<T> {}
-// }
+impl _Expression {
+    pub fn evaluate(&self) -> _Atom {
+        match self.op {
+            OperatorToken::Add => {
+                self.vars.iter().fold(_Atom::new(),
+                    |res, acc| _Atom { value: res.value + acc.value }
+                )
+            },
+            _ => unimplemented!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod evaluate_tests {
+    use parse::lisp_ast::{_Atom, _Expression};
+    use lisp_token::OperatorToken;
+
+    #[test]
+    fn expression_evaluates_one_plus_two() {
+        let expr = _Expression {
+            op: OperatorToken::Add,
+            vars: vec![
+                _Atom { value: 1_i64 },
+                _Atom { value: 2_i64 },
+            ],
+        };
+        let expected = _Atom { value: 3_i64 };
+        let result = expr.evaluate();
+        assert_eq!(result, expected);
+    }
+}
