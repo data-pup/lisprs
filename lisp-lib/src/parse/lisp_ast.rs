0940e0817e33a1b3d53ce5f4cfb3179c6fd333a3
data-pup <kevinmartinucr@gmail.com>
2018-04-10 11:49:41 -0700

    Addded some test cases for teasing out some logic for working with the token stack given to the AST.

--- lisp-lib/src/parse/lisp_ast.rs
+++ lisp-lib/src/parse/lisp_ast.rs
@@ -38,6 +38,15 @@ impl TryFrom<Vec<LispToken>> for _LispAstNode {
 }
 
 impl _LispAstNode {
+    /// Pop tokens off of the stack, and return the tokens that form the next
+    /// expression in the stack.
+    fn get_next_expr(token_stack: &mut Vec<LispToken>)
+        -> Result<Vec<LispToken>,_ParseError> {
+        let mut expr = vec![];
+        while let Some(token) = token_stack.pop() { expr.push(token) }
+        Ok(expr)
+    }
+
     // Create an operator node. If args were given, return a parse error.
     fn create_op_node(op: LispOperator, args: Vec<_LispAstNode>)
         -> Result<_LispAstNode, _ParseError> {
@@ -144,5 +153,30 @@ mod parse_tests {
             assert!(result.is_err());
             assert_eq!(result.unwrap_err(), _ParseError::EmptyExpression);
         }
+    }
+}
+
+#[cfg(test)]
+mod get_next_expr_tests {
+    use parse::lisp_ast;
+    use parse::_ParseError;
+    use lisp_token::LispToken;
+    use lisp_operator::LispOperator;
+
+    #[test]
+    fn unwrapped_expression_is_returned_unchanged() {
+        let mut input = vec![ // + 1 2
+            LispToken::Operator(LispOperator::Add),
+                LispToken::Value(String::from("1")),
+                LispToken::Value(String::from("2")),
+        ];
+        let expected = vec![
+            LispToken::Operator(LispOperator::Add),
+                LispToken::Value(String::from("1")),
+                LispToken::Value(String::from("2")),
+        ];
+        let output = lisp_ast::_LispAstNode::get_next_expr(&mut input);
+        assert!(output.is_ok());
+        assert_eq!(output.unwrap(), expected);
     }
 }
