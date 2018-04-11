4cde5e20a717cc7ba6ecab69173dd6fa86697710
data-pup <kevinmartinucr@gmail.com>
2018-04-11 12:58:50 -0700

    Split the children tests apart, the whole reason behind the previous typo was due to these being within a single function.

--- lisp-lib/src/parse/tests/test_lisp_ast_has_children.rs
+++ lisp-lib/src/parse/tests/test_lisp_ast_has_children.rs
@@ -5,19 +5,25 @@ mod test_lisp_ast_has_children {
     use parse::lisp_ast::LispAstNode;
 
     #[test]
-    fn has_children_works() {
+    fn node_with_none_returns_false() {
         let none_children = LispAstNode {
             token: LispToken::Value(String::from("1")),
             children: None,
         };
         assert_eq!(none_children.has_children(), false);
+    }
 
+    #[test]
+    fn node_with_empty_children_vector_returns_false() {
         let empty_children = LispAstNode {
             token: LispToken::Value(String::from("1")),
-            children: None,
+            children: Some(vec![]),
         };
         assert_eq!(empty_children.has_children(), false);
+    }
 
+    #[test]
+    fn node_with_children_returns_true() {
         let one_child = LispAstNode {
             token: LispToken::Operator(LispOperator::Add),
             children: Some(vec![
