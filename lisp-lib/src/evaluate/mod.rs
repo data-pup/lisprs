95f68d2e8180367a05a554ec52744d9c3ce45397
data-pup <kevinmartinucr@gmail.com>
2018-04-11 12:32:18 -0700

    Added some scaffolding for the evaluation process. Variables, types, etc. will be a later concern. For now, I am pretending this is JS and only using one number type.

--- lisp-lib/src/evaluate/mod.rs
+++ lisp-lib/src/evaluate/mod.rs
@@ -1,5 +1,27 @@
 use parse::LispAstNode;
 
+enum EvaluateError {
+    TempErrorName,
+}
+
 pub fn evaluate(ast: LispAstNode) -> String {
+    match evaluate_helper(ast) {
+        Ok(res) => unimplemented!(),
+        Err(_) => String::from("Error!"),
+    }
+}
+
+fn evaluate_helper(ast: LispAstNode) -> Result<f64, EvaluateError> {
+    match ast.has_children() {
+        true => evaluate_expr(ast),
+        false => evaluate_leaf_node(ast),
+    }
+}
+
+fn evaluate_expr(_node: LispAstNode) -> Result<f64, EvaluateError> {
+    unimplemented!();
+}
+
+fn evaluate_leaf_node(_node: LispAstNode) -> Result<f64, EvaluateError> {
     unimplemented!();
 }
