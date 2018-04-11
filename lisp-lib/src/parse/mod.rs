54a84ef6cde9361c59670c2695411441039cd3f3
data-pup <kevinmartinucr@gmail.com>
2018-04-11 11:57:31 -0700

    Parse function shooould just end up being a call to try_from, since that will take care of error handling internally.

--- lisp-lib/src/parse/mod.rs
+++ lisp-lib/src/parse/mod.rs
@@ -1,14 +1,15 @@
-use lisp_token::LispToken;
-
 mod lisp_ast;
 mod lisp_ast_from_tokens;
 mod parse_errors;
 mod tests;
+
+use std::convert::TryFrom;
+use lisp_token::LispToken;
 
 pub use self::parse_errors::_ParseError;
 pub use self::lisp_ast::_LispAstNode;
 
 // Parse a vector of tokens, evaluate the expression, and return the result.
 pub fn parse(_tokens: Vec<LispToken>) -> Result<_LispAstNode, _ParseError> {
-    unimplemented!();
+    _LispAstNode::try_from(_tokens)
 }
