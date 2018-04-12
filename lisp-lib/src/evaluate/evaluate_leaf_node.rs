ab52e4d6344febe964a72dfa842f8c51e0bc9b24
data-pup <kevinmartinucr@gmail.com>
2018-04-11 18:21:03 -0700

    Urg.

--- lisp-lib/src/evaluate/evaluate_leaf_node.rs
+++ lisp-lib/src/evaluate/evaluate_leaf_node.rs
@@ -10,7 +10,7 @@ pub fn evaluate_leaf(node: LispAstNode) -> Result<f64, EvalError> {
         Variable(_) => unimplemented!(),
         Value(val_str) => {
             let val = val_str.parse::<f64>();
-            if val.is_ok() { Ok(val.unwrap())                    }
+            if val.is_ok() { Ok(val.unwrap())                }
             else           { Err(EvalError::ValueParseError) }
         },
         _ => Err(EvalError::InvalidLeafNode),
